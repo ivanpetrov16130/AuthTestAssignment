c16ad77376234890a4ada01997f4d4833a1c95ab
Ilya Boikov <boikov@infoshell.mobi>
2018-04-18 21:04:18 +0300

    Initial Commit

---
+++ AuthTestAssignmentTests/AuthTestAssignmentTests.swift
@@ -0,0 +1,36 @@
+//
+//  AuthTestAssignmentTests.swift
+//  AuthTestAssignmentTests
+//
+//  Created by Ivan on 18.04.2018.
+//  Copyright © 2018 Ivan Petrov. All rights reserved.
+//
+
+import XCTest
+@testable import AuthTestAssignment
+
+class AuthTestAssignmentTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
